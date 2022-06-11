c366e1f375f7aaa61c98f8e7bb22bf609e2783a4
vonfreiren <100447524+vonfreiren@users.noreply.github.com>
2022-06-11 14:48:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os, sys
+import glob
+
+filetype="jpg"
+filetype2="webp"
+
+path = "/Users/mariajosealvarez/IdeaProjects/vonfreiren.github.io/img/posts"
+files = glob.glob(path + '/**/*.jpg', recursive=True)
+
+
+def resize():
+    for item in files:
+        try:
+            im = Image.open(item)
+            f, e = os.path.splitext(item)
+            imResize = im.resize((1920,1220), Image.ANTIALIAS)
+            imResize.save(f+'.jpg', 'JPEG', quality=85)
+        except:
+            print(item+" cannot be saved")
+resize()
