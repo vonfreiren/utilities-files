cf25ff486e051f41a479fbc5398a1bf698774d59
vonfreiren <100447524+vonfreiren@users.noreply.github.com>
2023-01-11 22:59:52 +0100

    Changes Project Structure

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ import os, sys
 import glob
 from delete_movies_files import delete
 
-filetype="jpg"
+filetype="jpeg"
 filetype2="webp"
 
 path = "/Users/mariajosealvarez/IdeaProjects/vonfreiren.github.io/img/posts"
+path = "/Users/mariajosealvarez/IdeaProjects/vonfreiren.github.io/img/posts/nomad"
+
 files = glob.glob(path + '/**/*.jpg', recursive=True)
 
 
 def resize():
     for item in files:
         try:
+            print(item)
             im = Image.open(item)
             f, e = os.path.splitext(item)
             imResize = im.resize((1920,1220), Image.ANTIALIAS)
+            print(item)
             imResize.save(f+'.jpg', 'JPEG', quality=85)
         except:
             print(item+" cannot be saved")
-#resize()
+resize()
 
-delete()
+#delete()
 
 
